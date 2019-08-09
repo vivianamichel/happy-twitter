ce3b8b37b43c817cecf8e9ca3b23c0ccc786ccf9
Viviana Michel <vivianamichel@Vivianas-MBP.lan>
2019-08-09 12:09:00 +0100

    initial commit with sentiment and angular fire config.

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyASg0Alqz-pYp5pWs2xF8TZvl9AnZtChTU",
+    authDomain: "happy-twitter-886ab.firebaseapp.com",
+    databaseURL: "https://happy-twitter-886ab.firebaseio.com",
+    projectId: "happy-twitter-886ab",
+    storageBucket: "happy-twitter-886ab.appspot.com",
+    messagingSenderId: "902680730155",
+    appId: "1:902680730155:web:cc5859a3c7b878ff"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
